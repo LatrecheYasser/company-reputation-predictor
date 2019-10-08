88b9d3a7bfcc36c7636302a29deb9881aa7a661b
taki_eddine <ft_mekhalfa@esi.dz>
2019-10-08 13:05:37 +0200

    Add newspaper scraper.

---
+++ Collector/ELK/scraper.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+import sys
+from newspaper import Article
+
+url = sys.argv[1]
+
+article = Article(url)
+article.download()
+if article.is_valid_url():
+    article.parse()
+    print(article.text)
+else:
+    print("Error downloading article: " + url, file=sys.stderr)
