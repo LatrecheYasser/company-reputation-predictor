2f771ac7fdf48e023e341a213ee4ead4fe1fe609
ghalib2015 <ghalibkenzi15@gmail.com>
2019-10-13 19:23:51 +0200

    Add rss links filter.

--- rssParser.py
+++ rssParser.py
@@ -1,10 +1,21 @@
+import feedparser
+from newspaper import Article
+
 fileOut = open("logStash.conf", 'w')
 fileOut.writelines("input {\n")
 
 with open("rssLinks.txt", "r") as fileIn:
     for line in fileIn:
         if "http" in line:
-            fileOut.writelines("\n".join(('	rss {', '		interval => 3600', '		url => "' + line[:-1] + '"', '	}\n')))
+            try:
+                feed = feedparser.parse(line)
+                post = feed.entries[0]
+                article = Article(url=post.link, language="fr")
+                article.download()
+                article.parse()
+                fileOut.writelines("\n".join(('	rss {', '		interval => 3600', '		url => "' + line[:-1] + '"', '	}\n')))
+            except Exception as e:
+                print(line+" : "+str(e))
 
 fileOut.writelines("}\n")
 
